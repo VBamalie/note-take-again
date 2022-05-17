b8f19c98c4082f3e3bcb9ede749a8bfe29e57fb6
Amalie Von Behren <vbamalie@gmail.com>
2022-05-17 17:18:48 -0400

    notes get works

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const router = require("express").Router();
-const {readFromFile , writeToFile, readAndAppend} = ("../helpers/helper.js")
+const {readFromFile , writeToFile, readAndAppend} = require("../helpers/helper.js")
 
 router.get('/notes', (req, res)=>{
     console.info("recieved notes")
-    readFromFile("../db/db.json").then((data)=> res.json(data))
+    readFromFile("./db/db.json").then((data)=> res.json(JSON.parse(data)));
 });
 
-router.post("/", )
+router.post("/")
+
+module.exports = router
