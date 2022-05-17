6932c31006477cb51c98c5f806881d3d0f8598a8
Amalie Von Behren <vbamalie@gmail.com>
2022-05-17 17:34:25 -0400

    post route works

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,25 @@
 const path = require("path");
 const router = require("express").Router();
 const {readFromFile , writeToFile, readAndAppend} = require("../helpers/helper.js")
+const uuid = require("../helpers/uuid")
 
 router.get('/notes', (req, res)=>{
     console.info("recieved notes")
     readFromFile("./db/db.json").then((data)=> res.json(JSON.parse(data)));
 });
 
-router.post("/")
+router.post("/notes", (req, res)=> {
+    console.log("adding note....")
+    const {title, text} = req.body;
+    if (req.body){
+        const newNote = {
+            title,
+            text,
+            note_id: uuid()
+        }
+        readAndAppend(newNote, "./db/db.json");
+        res.json("new note added succesfully")
+    } else res.error("error adding note")
+})
 
 module.exports = router
